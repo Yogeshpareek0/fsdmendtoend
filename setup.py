c090a8ac91746687fa24ffbdafb18c1908dcce27
yogesh pareek <ykp900155@gmail.com>
2023-10-31 00:28:13 +0530

    template addes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Diamondpriceprediction',
+    version='0.0.1',
+    author='yogesh pareek',
+    author_email='ykp900155@gmail.com',
+    packages=find_packages()
+)
